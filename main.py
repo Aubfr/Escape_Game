d9c47f2c1c74a6d478199e74630c2718d8b0ef85
Aubfr <aubin.knittel@icloud.com>
2025-09-24 19:34:45 +0200

    uddate .py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # ESCAPE GAME DEV BY AUB 23/09/2025
 from termcolor import colored
+import time
 
 ascii_aub = """
   _____  ________      __    ______     __          _    _ ____  
@@ -10,4 +11,16 @@ ascii_aub = """
  |_____/|______|   \/       |____/  |_|    /_/    \_\____/|____/ 
 """
 print(ascii_aub)
-print("Bienvenue à vous ! ")
+print("\n")
+
+#Histoire
+
+print(colored("Bienvenue à vous jeune(s) aventuriers(s) !", "red"))
+time.sleep(1)
+print("Thème : “Mission Spatiale : Sauver la Station Orbitale”")
+time.sleep(0,5)
+print("Voici le contexte : ")
+time.sleep(0,5)
+print("Tu es un astronaute coincé dans une station spatiale en orbite autour de Mars.")
+time.sleep(1)
+print("Une panne a bloqué tous les systèmes et tu dois réparer la station et retrouver le module de survie avant que l’oxygène ne s’épuise !")
